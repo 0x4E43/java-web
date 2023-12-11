205168da9e87b729e98c634a2273efd4a25583c2
0x4E43 <nimaic.dev@gmail.com>
2023-12-11 16:05:59 +0530

    Commement added to test git branch

--- src/main/java/com/thahiba/nimaitest/service/UserService.java
+++ src/main/java/com/thahiba/nimaitest/service/UserService.java
@@ -13,25 +13,27 @@ public class UserService {
 
     @Autowired
     private UserDAO userDAO;
-    public UserEntity createUser(UserDTO dto){
-        try{
+
+    public UserEntity createUser(UserDTO dto) {
+        try {
             UserEntity entity = new UserEntity();
             entity.setName(dto.getName());
             entity.setEmail(dto.getEmail());
             entity.setPassword(dto.getPassword());
             entity = userDAO.save(entity);
             return entity;
-        }catch (Exception e){
+        } catch (Exception e) {
             e.printStackTrace();
             return null;
         }
     }
 
     public List<UserEntity> getAllUsers() {
-        try{
+        try {
+            // This function will return list of all users
             List<UserEntity> userEntityList = userDAO.findAll();
             return userEntityList;
-        }catch(Exception e){
+        } catch (Exception e) {
             e.printStackTrace();
             return null;
         }
